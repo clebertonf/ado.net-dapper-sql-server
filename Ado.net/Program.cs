725d84188e5ab3599ef3d0b5ae83ff6ff39927eb
Cleberton Carvalho <clebertonfgc@gmail.com>
2022-11-23 10:53:30 -0300

    Finalizando ADO.net

--- Ado.net/Program.cs
+++ Ado.net/Program.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
 
-namespace Ado.net
+namespace DataAccessProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string connectionString = "Server=localhost,1433;Database=balta;Encrypt=False;User ID=sa;Password=1q2w3e4r@#$";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                System.Console.WriteLine("Successfully connected to the database");
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [title] FROM [Course]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        System.Console.WriteLine(reader.GetString(0));
+                    }
+                };
+            }
         }
     }
 }
