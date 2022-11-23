6fefc06433898fe40611f8dfca21231ad6734024
Cleberton Carvalho <clebertonfgc@gmail.com>
2022-11-23 15:05:37 -0300

    Restruturando projeto em pastas

---
+++ EstudoDapper/Métodos Base Dapper/MetodosBaseDapper.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using EstudoDapper.Models;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace EstudoDapper.Métodos_Base
+{
+    public static class MetodosBaseDapper
+    {
+        public static void ListCategories(SqlConnection connection)
+        {
+            // select
+            var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+
+        public static void InsertCategorie(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços do AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO 
+                [Category]
+             VALUES(
+                  @Id,
+                  @Title,
+                  @Url,
+                  @Summary,
+                  @Order,
+                  @Description,
+                  @Featured)";
+
+            // Insert
+            var rows = connection.Execute(insertSql, new
+            {
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+
+            Console.WriteLine(rows);
+        }
+
+        public static void UpdateCategorie(SqlConnection connection)
+        {
+
+        }
+    }
+}
--- EstudoDapper/Program.cs
+++ EstudoDapper/Program.cs
@@ -1,6 +1,5 @@
-﻿using Dapper;
+﻿using EstudoDapper.Métodos_Base;
 using Microsoft.Data.SqlClient;
-using System;
 
 namespace DataAccessProject
 {
@@ -10,51 +9,10 @@ namespace DataAccessProject
         {
             const string connectionString = "Server=localhost,1433;Database=balta;Encrypt=False;User ID=sa;Password=1q2w3e4r@#$";
 
-            var category = new Category();
-            category.Id = Guid.NewGuid();
-            category.Title = "Amazon AWS";
-            category.Url = "amazon";
-            category.Description = "Categoria destinada a serviços do AWS";
-            category.Order = 8;
-            category.Summary = "AWS Cloud";
-            category.Featured = false;
-
-            var insertSql = @"INSERT INTO 
-                [Category]
-             VALUES(
-                  @Id,
-                  @Title,
-                  @Url,
-                  @Summary,
-                  @Order,
-                  @Description,
-                  @Featured)";
-
-
             using (var connection = new SqlConnection(connectionString))
             {
-
-
-                // Insert
-                var rows = connection.Execute(insertSql, new
-                {
-                    category.Id,
-                    category.Title,
-                    category.Url,
-                    category.Summary,
-                    category.Order,
-                    category.Description,
-                    category.Featured
-                });
-
-                Console.WriteLine(rows);
-
-                // select
-                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
-                foreach (var item in categories)
-                {
-                    Console.WriteLine(item.Title);
-                }
+                MetodosBaseDapper.ListCategories(connection);
+                MetodosBaseDapper.InsertCategorie(connection);
             }
         }
     }
