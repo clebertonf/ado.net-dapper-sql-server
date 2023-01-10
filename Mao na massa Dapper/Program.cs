81e40e247e9c249564164216e92b21c754aa2a42
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-01-09 22:38:21 -0300

    Creating hands-on project with dapper

---
+++ Mao na massa Dapper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mao_na_massa_Dapper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
