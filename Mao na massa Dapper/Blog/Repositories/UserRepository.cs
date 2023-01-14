d16a4572119f80d4cca8b112ee3d370b7d5bb71c
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-01-14 19:06:29 -0300

    Add methods and share connection

--- Mao na massa Dapper/Blog/Repositories/UserRepository.cs
+++ Mao na massa Dapper/Blog/Repositories/UserRepository.cs
@@ -1,18 +1,31 @@
 ﻿using Dapper.Contrib.Extensions;
 using Mao_na_massa_Dapper.Blog.Models;
 using Microsoft.Data.SqlClient;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Mao_na_massa_Dapper.Blog.Repositories
 {
-    public class UserRepository
-    {
+    public class UserRepository 
+    { 
+        private SqlConnection _connection = new SqlConnection();
         public IEnumerable<User> GetAll(string connectionString)
         {
-            using var connection = new SqlConnection(connectionString);
-            return connection.GetAll<User>();
+            return _connection.GetAll<User>();
+        }
+
+        public User Get(int id)
+        {
+            return _connection.Get<User>(id);
+        }
+
+        public void Create(User user)
+        {
+            _connection.Insert<User>(user);
+        }
+
+        public void Delete(User user)
+        {
+            _connection.Delete<User>(user);
         }
     }
 }
