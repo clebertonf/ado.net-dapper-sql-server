9aed86cdbb5fe2a176b750ca6d3d676ef7c262cb
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-01-16 23:05:45 -0300

    Add Role model and Role repository

---
+++ Mao na massa Dapper/Blog/Models/Role.cs
@@ -0,0 +1,15 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mao_na_massa_Dapper.Blog.Models
+{
+    [Table("[Role]")]
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+    }
+}
---
+++ Mao na massa Dapper/Blog/Repositories/RoleRepository.cs
@@ -0,0 +1,44 @@
+﻿using Dapper.Contrib.Extensions;
+using Mao_na_massa_Dapper.Blog.Models;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mao_na_massa_Dapper.Blog.Repositories
+{
+    public class RoleRepository
+    {
+        private readonly SqlConnection _connection;
+
+        public RoleRepository(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Role> GetAll()
+        {
+            return _connection.GetAll<Role>();
+        }
+
+        public Role Get(int id)
+        {
+            return _connection.Get<Role>(id);
+        }
+
+        public void Create(Role role)
+        {
+            _connection.Insert<Role>(role);
+        }
+
+        public void Update(Role role)
+        {
+            _connection.Update<Role>(role);
+        }
+
+        public void Delete(Role role)
+        {
+            _connection.Delete<Role>(role);
+        }
+    }
+}
