fb0fd6b3e4091139a9e359652fc91cb62f961422
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-01-17 22:24:17 -0300

    Add model category

--- Mao na massa Dapper/Blog/Models/Category.cs
+++ Mao na massa Dapper/Blog/Models/Category.cs
@@ -1,12 +1,12 @@
-﻿using Dapper.Contrib.Extensions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Dapper.Contrib.Extensions;
 
 namespace Mao_na_massa_Dapper.Blog.Models
 {
-    [Table("[Tag]")]
-    public class Tag
+    [Table("[Category]")]
+    public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
