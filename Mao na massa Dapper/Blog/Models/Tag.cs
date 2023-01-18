1aac2e404d487acbf001e1f102a1b9ca2ddbb906
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-01-17 22:21:53 -0300

    Add Model Tag

---
+++ Mao na massa Dapper/Blog/Models/Tag.cs
@@ -0,0 +1,18 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mao_na_massa_Dapper.Blog.Models
+{
+    [Table("[Tag]")]
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string slug { get; set; }
+
+        [Write(false)]
+        public List<Post> Posts { get; set; }
+    }
+}
