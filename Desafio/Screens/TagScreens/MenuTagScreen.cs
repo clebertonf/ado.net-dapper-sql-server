2ebf1750ad642bdb3da6eef1df944a6c106b6a43
Cleberton Carvalho <clebertonfgc@gmail.com>
2023-01-22 17:51:18 -0300

    Add menu Tag Screen

---
+++ Desafio/Screens/TagScreens/MenuTagScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desafio.Screens.TagScreens
+{
+    public static class MenuTagScreen
+    {
+        public static void load()
+        {
+            Console.Clear();
+            Console.WriteLine("Gestão de tags");
+            Console.WriteLine("--------------");
+            Console.WriteLine("O que deseja fazer?");
+            Console.WriteLine();
+            Console.WriteLine("1 - Listar tags");
+            Console.WriteLine("2 - Cadastrar tags");
+            Console.WriteLine("3 - Atualizar tag");
+            Console.WriteLine("4 - Excluir tag");
+            Console.WriteLine();
+            Console.WriteLine();
+            var option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    break;
+            }
+        }
+    }
+}
